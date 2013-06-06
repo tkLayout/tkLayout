e49331444fe42706a11df40dbbba0f4c9ecc767a
stefano.mersi@gmail.com <stefano.mersi@cern.ch>
2013-06-06 00:41:35 +0000

    Palette changed to favour PS modules and stabilixe pixels.

--- src/Palette.cc
+++ src/Palette.cc
@@ -7,11 +7,12 @@ std::map<std::string, int> Palette::colorPickMap;
 void Palette::initializeMe() {
   Palette::colorPickMap["ptOut"] = 2;
   Palette::colorPickMap["rphi"] = 4;
-  Palette::colorPickMap["stereo"] = 1;
+  Palette::colorPickMap["stereo"] = 6;
   Palette::colorPickMap["ptIn"] = 9;
-  Palette::colorPickMap["ptMixed"] = 6;
+  Palette::colorPickMap["ptMixed"] = 1;
   Palette::colorPickMap["pt2SLong"] = 11;
   Palette::colorPickMap["pt2SShort"] = 4;
+  Palette::colorPickMap["pixel"] = 9;
   initialized = true;
 }
 
