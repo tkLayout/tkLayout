7fab659f0d879880573a3874e589b1943d9aa9dc
stefano.martina@gmail.com <stefano.martina@gmail.com>
2014-03-11 10:42:20 +0000

    added setup_slc6.sh for configuring the working ambient

---
+++ setup_slc6.sh
@@ -0,0 +1,6 @@
+source /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.09/x86_64-slc6-gcc47-dbg/root/bin/thisroot.sh
+source /afs/cern.ch/sw/lcg/contrib/gcc/4.7/x86_64-slc6-gcc47-opt/setup.sh
+export LD_LIBRARY_PATH=/afs/cern.ch/sw/lcg/external/Boost/1.55.0_python2.7/x86_64-slc6-gcc47-opt/lib:$LD_LIBRARY_PATH
+export BOOST_LIB=/afs/cern.ch/sw/lcg/external/Boost/1.55.0_python2.7/x86_64-slc6-gcc47-opt/lib
+export BOOST_INCLUDE=/afs/cern.ch/sw/lcg/external/Boost/1.55.0_python2.7/x86_64-slc6-gcc47-opt/include/boost-1_55
+export BOOST_SUFFIX=-gcc47-mt-1_55 
