ee20329147a0c2939a5f330568b82e720e9f63e1
Zbynek Drasal <Zbynek.Drasal@cern.ch>
2015-06-08 18:06:13 +0200

    Finished CMake versioning - install works within Cmake

--- GetGitVersion.cmake
+++ GetGitVersion.cmake
@@ -1,23 +1,61 @@
 FIND_PACKAGE(Git)
 IF(GIT_FOUND)
-  #EXECUTE_PROCESS(COMMAND ${GIT_EXECUTABLE} describe --tags RESULT_VARIABLE res_var OUTPUT_VARIABLE GIT_COM_ID )
-  EXECUTE_PROCESS(COMMAND ${GIT_EXECUTABLE} rev-parse HEAD RESULT_VARIABLE res_var OUTPUT_VARIABLE GIT_COM_ID )
-  IF( NOT ${res_var} EQUAL 0 )
-    SET( GIT_COMMIT_ID "git commit id unknown")
-    MESSAGE( WARNING "Git failed (not a repo, or no tags). Build will not contain git revision info." )
+
+  # Get Git branch
+  EXECUTE_PROCESS(COMMAND ${GIT_EXECUTABLE} rev-parse --abbrev-ref HEAD RESULT_VARIABLE res_var_branch OUTPUT_VARIABLE GIT_BRANCH )
+  IF( NOT ${res_var_branch} EQUAL 0 )
+    SET( GIT_BRANCH "Git_Command_Failed")
+    MESSAGE( WARNING "Git branch failed (not a repo, or no tags). Build will not contain git revision info." )
   ENDIF()
-  STRING( REPLACE "\n" "" GIT_COMMIT_ID ${GIT_COM_ID} )
-ELSE()
-  SET( GIT_COMMIT_ID "unknown (git not found!)")
-  MESSAGE( WARNING "Git not found. Build will not contain git revision info." ) 
+ 
+  # Get Git version
+  IF(CMAKE_HOST_UNIX) 
+    # Unix like system
+#   EXECUTE_PROCESS(COMMAND ${GIT_EXECUTABLE} rev-list HEAD | wc -l RESULT_VARIABLE res_var OUTPUT_VARIABLE GIT_VERSION )
+    EXECUTE_PROCESS(COMMAND bash -c "git rev-list HEAD | wc -l" RESULT_VARIABLE res_var_version OUTPUT_VARIABLE GIT_VERSION )
+    
+  ELSE()
+    # Non-unix system
+    EXECUTE_PROCESS(COMMAND ${GIT_EXECUTABLE} rev-parse HEAD RESULT_VARIABLE res_var_version OUTPUT_VARIABLE GIT_VERSION )
+  ENDIF()
+  IF( NOT ${res_var_version} EQUAL 0 )
+    SET( GIT_VERSION "Git_Command_Failed")
+    MESSAGE( WARNING "Git version failed (not shell, not a repo, or no tags). Build will not contain git revision info." )
+  ENDIF()
+
+  # Get Git author
+  EXECUTE_PROCESS(COMMAND ${GIT_EXECUTABLE} config --get remote.origin.url RESULT_VARIABLE res_var_author OUTPUT_VARIABLE GIT_AUTHOR )
+  IF( NOT ${res_var_author} EQUAL 0 )
+    SET( GIT_AUTHOR "Git_Command_Failed")
+    MESSAGE( WARNING "Git config url failed (not a repo, or no tags). Build will not contain git revision info." )
+  ENDIF()
+
+  STRING( REPLACE "\n" "" GIT_BRANCH  ${GIT_BRANCH}  )
+  STRING( REPLACE "\n" "" GIT_VERSION ${GIT_VERSION} )
+  STRING( REPLACE "\n" "" GIT_AUTHOR  ${GIT_AUTHOR}  )
+ 
+  # All information found
+  IF ( (${res_var_branch} EQUAL 0) AND (${res_var_version} EQUAL 0) AND (${res_var_author} EQUAL 0) ) 
+    STRING( CONCAT GIT_REVISION ${GIT_BRANCH} "-" ${GIT_VERSION} " on " ${GIT_AUTHOR} )
+
+    MESSAGE( STATUS "")
+    MESSAGE( STATUS "Git Revision: ${GIT_REVISION} " )
+    MESSAGE( STATUS "" )
+
+    # Unix like system
+    IF(CMAKE_HOST_UNIX)
+  
+      # Create new SvnRevision.cpp
+      SET( vstring "#include <SvnRevision.h>\n\n"
+                   "//SvnRevision.cpp - written by cmake. changes will be lost!\n"
+                   "const std::string SvnRevision::revisionNumber = \"${GIT_REVISION}\"\;\n")
+
+      FILE(WRITE SvnRevision.cpp.txt ${vstring} )
+      EXECUTE_PROCESS(COMMAND bash -c "if [ ! -e SvnRevision.orig.cpp ]; then cp -p ${CMAKE_CURRENT_SOURCE_DIR}/../src/SvnRevision.cpp SvnRevision.orig.cpp; fi" )
+      EXECUTE_PROCESS(COMMAND ${CMAKE_COMMAND} -E copy_if_different
+                              SvnRevision.cpp.txt ${CMAKE_CURRENT_SOURCE_DIR}/../src/SvnRevision.cpp)
+    ENDIF()
+  ENDIF()
 ENDIF()
 
-SET( vstring "#include <SvnRevision.h>\n\n"
-             "//SvnRevision.cpp - written by cmake. changes will be lost!\n"
-             "const std::string SvnRevision::revisionNumber = \"${GIT_COMMIT_ID}\"\;\n")
 
-FILE(WRITE SvnRevision.cpp.txt ${vstring} )
-# copy the file to the final header only if the version changes
-# reduces needless rebuilds
-EXECUTE_PROCESS(COMMAND ${CMAKE_COMMAND} -E copy_if_different
-                        SvnRevision.cpp.txt ${CMAKE_CURRENT_SOURCE_DIR}/../src/SvnRevision.cpp)
--- src/SvnRevision.cpp
+++ src/SvnRevision.cpp
@@ -1,4 +1,4 @@
 #include <SvnRevision.h>
 
 //SvnRevision.cpp - written by cmake. changes will be lost!
-const std::string SvnRevision::revisionNumber = "1335fae96aa02c0ae5c4c355dca19f878d317024";
+const std::string SvnRevision::revisionNumber = "NotDefined";
