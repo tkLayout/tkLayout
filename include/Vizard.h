542f845fa5b725b5aabe66e0c187df00ffedd5fe
Stefano Mersi <stefano.mersi@cern.ch>
2016-04-12 18:26:31 +0200

    Adding the error plot for eta (fixes #90) Fixing a compilation error

--- include/Vizard.h
+++ include/Vizard.h
@@ -284,6 +284,7 @@ namespace insur {
 
     TProfile* newProfile(TH1D* nn);
     TProfile& newProfile(const TGraph& sourceGraph, double xlow, double xup, int nrebin = 1, int nBins = 0);
+    TProfile& newProfile_timesSin(const TGraph& sourceGraph, double xlow, double xup, int nrebin = 1, int nBins = 0);
     // int getNiceColor(unsigned int plotIndex);
     std::vector<Tracker*> trackers_;
     TCanvas* drawFullLayout();
