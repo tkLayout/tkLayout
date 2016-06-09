f215fff634bf4830c910ca99a926e6abce691319
Zbynek Drasal <Zbynek.Drasal@cern.ch>
2016-06-09 14:21:13 +0200

    Removed src/SvnRevision.cpp from git tracking to be compatible with devLite

--- src/SvnRevision.cpp
+++
@@ -1,4 +0,0 @@
-#include <SvnRevision.h>
-
-//SvnRevision.cpp - written by cmake. changes will be lost!
-const std::string SvnRevision::revisionNumber = "devLite-778 on git@github.com:drasal/tkLayout.git";
