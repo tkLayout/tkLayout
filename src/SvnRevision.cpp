42aafeed9a3486ecc09aa360c60b4e13407f8da3
Zbynek Drasal <Zbynek.Drasal@cern.ch>
2016-06-09 14:16:30 +0200

    Adding src/SvnRevision.cpp to be compatible with devLite

---
+++ src/SvnRevision.cpp
@@ -0,0 +1,4 @@
+#include <SvnRevision.h>
+
+//SvnRevision.cpp - written by cmake. changes will be lost!
+const std::string SvnRevision::revisionNumber = "devLite-778 on git@github.com:drasal/tkLayout.git";
