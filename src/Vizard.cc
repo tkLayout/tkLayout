62aff5d1f31e2117421e1017abd187aece93f548
Gabrielle Hugo <gabrielle.hugo83@gmail.com>
2016-08-10 11:24:07 +0200

    Auto tilted modules placement : modules sizes for flat part could only be specified under Layer cfg bloc and not Ring, now fixed. + zError commented in cfg file for BPX because zError is not defined for flat part, and not used by tilted part. + zError for Pixel flat part displayed as n/a on website, because undefined.

--- src/Vizard.cc
+++ src/Vizard.cc
@@ -1417,11 +1417,15 @@ namespace insur {
 	    tiltedPartTable->setContent(15, 0, "deltaZ" + subStart + "Outer" + subEnd + " (Ring i & i-1)");
 	    tiltedPartTable->setContent(15, i+1, l.tiltedRingsGeometryInfo().deltaZOuter()[ringNumber], coordPrecision);
 	    tiltedPartTable->setContent(16, 0, "phiOverlap");
-	    tiltedPartTable->setContent(16, i+1, l.tiltedRingsGeometry()[ringNumber]->phiOverlap(), coordPrecision);	    
+	    tiltedPartTable->setContent(16, i+1, l.tiltedRingsGeometry()[ringNumber]->phiOverlap(), coordPrecision);
+	    double zErrorInner = l.tiltedRingsGeometryInfo().zErrorInner()[ringNumber];	    
 	    tiltedPartTable->setContent(17, 0, "zError" + subStart + "Inner" + subEnd + " (Ring i & i-1)");
-	    tiltedPartTable->setContent(17, i+1, l.tiltedRingsGeometryInfo().zErrorInner()[ringNumber], coordPrecision);
+	    if (!std::isnan(zErrorInner)) tiltedPartTable->setContent(17, i+1, zErrorInner, coordPrecision);
+	    else tiltedPartTable->setContent(17, i+1, "n/a");
+	    double zErrorOuter = l.tiltedRingsGeometryInfo().zErrorOuter()[ringNumber];
 	    tiltedPartTable->setContent(18, 0, "zError" + subStart + "Outer" + subEnd + " (Ring i & i-1)");
-	    tiltedPartTable->setContent(18, i+1, l.tiltedRingsGeometryInfo().zErrorOuter()[ringNumber], coordPrecision);
+	    if (!std::isnan(zErrorOuter)) tiltedPartTable->setContent(18, i+1, zErrorOuter, coordPrecision);
+	    else tiltedPartTable->setContent(18, i+1, "n/a");	    
 	  }
 	  tiltedPartTables.push_back(tiltedPartTable);
 
@@ -1451,10 +1455,14 @@ namespace insur {
 	    flatPartTable->setContent(7, 0, "phiOverlap");
 	    flatPartTable->setContent(7, i+1, (((minusBigDeltaRod->zPlusParity() * pow(-1, (i%2))) > 0) ? l.flatPartPhiOverlapSmallDeltaPlus() : l.flatPartPhiOverlapSmallDeltaMinus()), coordPrecision);
 	    if (i > 0) {
+	      double zErrorInner = l.flatRingsGeometryInfo().zErrorInner()[i];
 	      flatPartTable->setContent(8, 0, "zError" + subStart + "Inner" + subEnd + " (Ring i & i-1)");
-	      flatPartTable->setContent(8, i+1, l.flatRingsGeometryInfo().zErrorInner()[i], coordPrecision);
+	      if (!std::isnan(zErrorInner)) flatPartTable->setContent(8, i+1, zErrorInner, coordPrecision);
+	      else flatPartTable->setContent(8, i+1, "n/a");
+	      double zErrorOuter = l.flatRingsGeometryInfo().zErrorOuter()[i];
 	      flatPartTable->setContent(9, 0, "zError" + subStart + "Outer" + subEnd + " (Ring i & i-1)");
-	      flatPartTable->setContent(9, i+1, l.flatRingsGeometryInfo().zErrorOuter()[i], coordPrecision);
+	      if (!std::isnan(zErrorOuter)) flatPartTable->setContent(9, i+1, zErrorOuter, coordPrecision);
+	      else flatPartTable->setContent(9, i+1, "n/a");
 	    }
 	    i++;
 	  }
