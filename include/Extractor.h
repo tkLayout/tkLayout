610f2bc81abbfe2eac97508c1bec6f6894d80f37
Gabrielle Hugo <gabrielle.hugo83@gmail.com>
2016-09-29 13:39:39 +0200

    Added comments in Extractor + Fixed error in a formula (which is not used anyway though).

--- include/Extractor.h
+++ include/Extractor.h
@@ -111,10 +111,9 @@ namespace insur {
     double compositeDensity(ModuleCap& mc, bool nosensors = false);
     double compositeDensity(InactiveElement& ie);
     double fromRim(double r, double w);
-    //int Z(double x0, double A);
-    double compute_X0(int pZ, double pA);
-    int findClosest_Z(double pA, double radiationLength);
     std::pair<double, int> getAZ(double radiationLength, double interactionLength);
+    int findClosest_Z(double pA, double radiationLength);
+    double compute_X0(int pZ, double pA); 
   };
 
 #if defined(__FLIPSENSORS_IN__) || defined(__FLIPSENSORS_OUT__)
