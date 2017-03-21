2fa5827a71cc6c8696500ae9ecf3b2af44a44515
Gabrielle Hugo <gabrielle.hugo83@gmail.com>
2017-03-21 15:07:49 +0100

    Added TiltedRing in Visitor

--- source/Tools/src/GitRevision.cc
+++ source/Tools/src/GitRevision.cc
@@ -1,4 +1,4 @@
 #include <GitRevision.h>
 
 //GitRevision.cc - written by cmake. changes will be lost!
-const std::string GitRevision::revisionNumber = "devLite-888 on git@github.com:ghugo83/tkLayout.git";
+const std::string GitRevision::revisionNumber = "devLite-889 on git@github.com:ghugo83/tkLayout.git";
