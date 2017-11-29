de22cf6ca37c9a4706f90509d6aa830652e5760c
Gabrielle Hugo <gabrielle.hugo83@gmail.com>
2017-11-29 12:28:14 +0100

    Bug fix in InactiveElement::checkTrackHits : there were hits computed out of volume.

--- source/Tools/src/GitRevision.cc
+++
@@ -1,4 +0,0 @@
-#include <GitRevision.h>
-
-//GitRevision.cc - written by cmake. changes will be lost!
-const std::string GitRevision::revisionNumber = "devLite-798 on git@github.com:drasal/tkLayout.git";
