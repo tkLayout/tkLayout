1137a21e12e7f318bbe901cf84414384d25709bc
Gabrielle Hugo <gabrielle.hugo83@gmail.com>
2018-09-04 17:24:24 +0200

    Additional removals of matSubdetectorName in cfg files

--- src/Endcap.cc
+++ src/Endcap.cc
@@ -78,7 +78,6 @@ void Endcap::build() {
       // Please note that the duplication is a rotation of axis CMS_Y and angle Pi !
       // This is because one does not want to build different disks for both sides, so no 'mirror' should be considered!
       Disk* diskn = GeometryFactory::clone(*diskp);
-      diskn->subdetectorName(myid());
       diskn->rotateToNegativeZSide();
 
       // Compute coverage on +Z side after built (TO DO : adapt for -Z side, and add a Tracker::computeActualCoverage(), completely independant from build() )
