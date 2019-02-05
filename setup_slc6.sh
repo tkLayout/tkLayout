76f897a55cef11e78910b4126a9ff95f36872403
Gabrielle Hugo <gabrielle.hugo83@gmail.com>
2019-02-05 17:29:54 +0100

    Phase-out of AFS: move all dependencies to CVMFS

--- setup_slc6.sh
+++ setup_slc6.sh
@@ -1,11 +1,13 @@
-source /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.09/x86_64-slc6-gcc47-dbg/root/bin/thisroot.sh
-source /afs/cern.ch/sw/lcg/contrib/gcc/4.7/x86_64-slc6-gcc47-opt/setup.sh
-export LD_LIBRARY_PATH=/afs/cern.ch/sw/lcg/external/Boost/1.55.0_python2.7/x86_64-slc6-gcc47-opt/lib:$LD_LIBRARY_PATH
-export BOOST_LIB=/afs/cern.ch/sw/lcg/external/Boost/1.55.0_python2.7/x86_64-slc6-gcc47-opt/lib
-export BOOST_INCLUDE=/afs/cern.ch/sw/lcg/external/Boost/1.55.0_python2.7/x86_64-slc6-gcc47-opt/include/boost-1_55
-export BOOST_SUFFIX=-gcc47-mt-1_55 
-export DOXYGEN_PATH=/afs/cern.ch/sw/lcg/external/doxygen/1.8.2/x86_64-slc6-gcc47-opt/bin
+source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/5.34.09/x86_64-slc6-gcc47-dbg/root/bin/thisroot.sh
 
+source /cvmfs/sft.cern.ch/lcg/external/gcc/4.7.2/x86_64-slc6-gcc47-opt/setup.sh
+
+export BOOST_INCLUDE=/cvmfs/sft.cern.ch/lcg/external/Boost/1.50.0_python2.7/x86_64-slc6-gcc47-opt/include/boost-1_50
+export BOOST_LIB=/cvmfs/sft.cern.ch/lcg/external/Boost/1.50.0_python2.7/x86_64-slc6-gcc47-opt/lib/
+export BOOST_SUFFIX=-gcc47-mt-1_50
+export LD_LIBRARY_PATH=/cvmfs/sft.cern.ch/lcg/external/Boost/1.50.0_python2.7/x86_64-slc6-gcc47-opt/lib:$LD_LIBRARY_PATH
+
+export DOXYGEN_PATH=/cvmfs/sft.cern.ch/lcg/external/doxygen/1.8.2/x86_64-slc6-gcc47-opt/bin
 if [ -d $DOXYGEN_PATH ] ; then
   export PATH=${DOXYGEN_PATH}:${PATH}
 fi
