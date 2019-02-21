d45b7bfe8a89746665cfcc80104a20c40b3af1f2
Gabrielle Hugo <gabrielle.hugo83@gmail.com>
2019-02-21 18:12:05 +0100

    Properly fixed merge conflict for CMake

--- setupCMake_slc6.sh
+++ setupCMake_slc6.sh
@@ -1,8 +1,7 @@
-source /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.09/x86_64-slc6-gcc47-dbg/root/bin/thisroot.sh
-source /afs/cern.ch/sw/lcg/contrib/gcc/4.7/x86_64-slc6-gcc47-opt/setup.sh
+source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/5.34.09/x86_64-slc6-gcc47-dbg/root/bin/thisroot.sh
+source /cvmfs/sft.cern.ch/lcg/external/gcc/4.7.2/x86_64-slc6-gcc47-opt/setup.sh
 export CC=`which gcc`
 export CXX=`which g++`
 export BOOST_INCLUDE=/cvmfs/sft.cern.ch/lcg/external/Boost/1.50.0_python2.7/x86_64-slc6-gcc47-opt/include/boost-1_50
 export BOOST_LIB=/cvmfs/sft.cern.ch/lcg/external/Boost/1.50.0_python2.7/x86_64-slc6-gcc47-opt/lib
 export BOOST_SUFFIX=-gcc47-mt-1_50
-
