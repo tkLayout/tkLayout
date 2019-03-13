fee79828c50ea0736e9be0f60805408041ac8fcb
Gabrielle Hugo <gabrielle.hugo83@gmail.com>
2019-03-13 12:49:00 +0100

    Cleaning on Makefile and CMakefile.

--- setupCMake_slc6.sh
+++ setupCMake_slc6.sh
@@ -1,7 +1,12 @@
-source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/5.34.09/x86_64-slc6-gcc47-dbg/root/bin/thisroot.sh
+# COMPILER
 source /cvmfs/sft.cern.ch/lcg/external/gcc/4.7.2/x86_64-slc6-gcc47-opt/setup.sh
 export CC=`which gcc`
 export CXX=`which g++`
+
+# ROOT
+source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/5.34.09/x86_64-slc6-gcc47-dbg/root/bin/thisroot.sh
+
+# BOOST
 export BOOST_LIB=/cvmfs/sft.cern.ch/lcg/external/Boost/1.50.0_python2.7/x86_64-slc6-gcc47-opt/lib
 export BOOST_INCLUDE=/cvmfs/sft.cern.ch/lcg/external/Boost/1.50.0_python2.7/x86_64-slc6-gcc47-opt/include/boost-1_50
 export BOOST_SUFFIX=-gcc47-mt-1_50
