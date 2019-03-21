13af09a6dad42b2605ae8ca1222fa473c01b9423
Gabrielle Hugo <gabrielle.hugo83@gmail.com>
2019-03-21 18:29:55 +0100

    Trick to avoid export of full VIEW_LCG/$ARCH-opt/lib64/ to LD_LIBRARY_PATH, which creates multiple regressions (fonts, emacs and so on).

--- setup_slc6.sh
+++ setup_slc6.sh
@@ -9,7 +9,7 @@ source $CONTRIB/gcc/8.2.0/$ARCH-opt/setup.sh
 
 # ROOT
 source $RELEASE_LCG/ROOT/6.16.00/$ARCH-dbg/bin/thisroot.sh
-export LD_LIBRARY_PATH=$VIEW_LCG/$ARCH-opt/lib64/:$VIEW_LCG/$ARCH-opt/lib/:$LD_LIBRARY_PATH
+#export LD_LIBRARY_PATH=$VIEW_LCG/$ARCH-opt/lib64/:$VIEW_LCG/$ARCH-opt/lib/:$LD_LIBRARY_PATH
 
 # BOOST
 export BOOST_INCLUDE=$RELEASE_LCG/Boost/1.69.0/$ARCH-opt/include
@@ -26,8 +26,21 @@ export PATH=`pwd`/bin:$PATH
 
 
 
+ cp /cvmfs/sft.cern.ch/lcg/views/LCG_95/x86_64-slc6-gcc8-opt/lib/libvdt.so . 
+ cp /cvmfs/sft.cern.ch/lcg/views/LCG_95/x86_64-slc6-gcc8-opt/lib64/libdavix.so.0 . 
+ cp /cvmfs/sft.cern.ch/lcg/views/LCG_95/x86_64-slc6-gcc8-opt/lib/libpng16.so.16 .
+ cp /cvmfs/sft.cern.ch/lcg/views/LCG_95/x86_64-slc6-gcc8-opt/lib/libASImage.so .
+ 
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_95/tbb/2019_U1/x86_64-slc6-gcc8-opt/tbb-env.sh
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_95/sqlite/3210000/x86_64-slc6-gcc8-opt/sqlite-env.sh
+
+#source /cvmfs/sft.cern.ch/lcg/releases/LCG_95/ROOT/6.16.00/x86_64-slc6-gcc8-opt/ROOT-env.sh 
+
+
+
+
 
 # FONT
-export FONTCONFIG_PATH=$VIEW_LCG/$ARCH-opt/etc/fonts/:$FONTCONFIG_PATH
+#export FONTCONFIG_PATH=$VIEW_LCG/$ARCH-opt/etc/fonts/:$FONTCONFIG_PATH
 #export FONTCONFIG_FILE
 #source /cvmfs/sft.cern.ch/lcg/views/LCG_95/x86_64-slc6-gcc8-opt/setup.sh 
