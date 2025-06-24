aa510d37bfd58f349cbde21a219ffa3732375afd
Stefano Mersi <stefano.mersi@cern.ch>
2025-06-24 17:03:34 +0200

    Fixing bug in ringPlace

--- src/ringPlace.cc
+++ src/ringPlace.cc
@@ -6,8 +6,9 @@ int main(int argc, char* argv[]) {
   if ((argc!=2)) { syntax(argv[0]); return(-1); };
 
   int nMods = atoi(argv[1]);
+  double nMods_d = nMods;
   for (int i =0; i<nMods; ++i) {
-    printf ("      Module %d {\n        manualPhiCenterDeg %.10f\n        yawAngleFromConfig 0\n      }\n", i, ((i/28.+.5) - int(i/28.+.5) - 0.5)*360 );
+    printf ("      Module %d {\n        manualPhiCenterDeg %.10f\n        yawAngleFromConfig 0\n      }\n", i, ((i/nMods_d+.5) - int(i/nMods_d+.5) - 0.5)*360 );
   }
 
   return 0;
